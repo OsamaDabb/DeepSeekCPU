0d494664fdb1d4b76bf09c377e67ddb8651190a4
Osama Dabbousi <dabbouob@login3.hsn.shaheen.hpc.kaust.edu.sa>
2025-02-27 12:13:43 +0300

    better torch import

--- inference.py
+++ inference.py
@@ -1,20 +1,20 @@
 import deepspeed
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch.distributed as dist
-import torch.device
+from torch import device
 
 model_path = "/scratch/reference/ai/models/LLMs/deepseek-r1"
 cache_dir = "/project/s10002/model"
-device = torch.device("cpu")
+cpu_device = torch.device("cpu")
 
-tokenizer = AutoTokenizer.from_pretrained(model_path, cache_dir=cache_dir, trust_remote_code=True).to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_path, cache_dir=cache_dir, trust_remote_code=True).to(cpu_device)
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
     trust_remote_code=True,
     cache_dir=cache_dir,
     device_map="cpu",
     torch_dtype="float32"
-).to(device)
+).to(cpu_device)
 
 inputs = tokenizer("Hello, how can I help you?", return_tensors="pt")
 outputs = model.generate(**inputs, max_new_tokens=50)
